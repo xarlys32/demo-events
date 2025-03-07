cbb8a75ef2bdf9949257468dcf0584b7be5e5bac
xarlys32 <57732203+xarlys32@users.noreply.github.com>
2025-03-07 22:34:05 +0100

    Changes

---
+++ src/main/java/com/fever/demo/config/Constants.java
@@ -0,0 +1,6 @@
+package com.fever.demo.config;
+
+public class Constants {
+    public static final String PROVIDER_BASE_URL = "https://provider.code-challenge.feverup.com/";
+    public static final String PROVIDER_EVENT_ENDPOINT = "api/events";
+}
--- src/main/java/com/fever/demo/infrastructure/repository/EventsProviderRepositoryImpl.java
+++ src/main/java/com/fever/demo/infrastructure/repository/EventsProviderRepositoryImpl.java
@@ -1,17 +1,31 @@
 package com.fever.demo.infrastructure.repository;
 
+import com.fever.demo.config.Constants;
 import com.fever.demo.domain.model.Events;
 import com.fever.demo.domain.port.EventsProviderRepository;
+import com.fever.demo.infrastructure.entity.XMLEvent;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
 @Component
 public class EventsProviderRepositoryImpl implements EventsProviderRepository {
 
-    @Scheduled(fixedRate = 1000)
+    private final WebClient webClient = WebClient.builder().baseUrl(Constants.PROVIDER_BASE_URL).build();
+
+    @Scheduled(fixedRate = 100000)
     public Optional<Events> lastEventsList() {
-        return Optional.empty();
+        webClient.get()
+                .uri(Constants.PROVIDER_EVENT_ENDPOINT)
+                .retrieve()
+                .bodyToFlux(XMLEvent.class)
+                .subscribe(this::mapToEvents);
+        return Optional.of(new Events());
+    }
+
+    private Events mapToEvents(XMLEvent xmlEvent) {
+        return new Events();
     }
 }
